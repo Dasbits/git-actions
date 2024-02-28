cba7302ca428966380a3ef632b9f6228fa2d0828
Dasbits <a20davsalsos@inspedralbes.cat>
2024-02-28 09:14:31 +0100

    eslint #1

--- vue-project/src/views/MainView.vue
+++ vue-project/src/views/MainView.vue
@@ -1,52 +1,55 @@
 <template>
-    <div class="container">
-        <button class="container_item" v-for="item in numbers" :key="item.id" @click="clickButton(item)">
-            {{ item }}
-        </button>
-    </div>
-    <p>{{ message }}</p>
+  <div class="container">
+    <button
+      class="container_item"
+      v-for="item in numbers"
+      :key="item.id"
+      @click="clickButton(item)"
+    >
+      {{ item }}
+    </button>
+  </div>
+  <p>{{ message }}</p>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-                message: ''
-
-            }
-        },
-        methods: {
-            clickButton(item) {
-                this.message = 'El butón presionat es el ' + item;
-            }
-        }
+export default {
+  data() {
+    return {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      message: ''
     }
+  },
+  methods: {
+    clickButton(item) {
+      this.message = 'El butón presionat es el ' + item
+    }
+  }
+}
 </script>
 
 <style scoped>
-
 .container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    padding: 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 20px;
 }
 
 .container_item {
-    background-color: #555;
-    color: #fff;
-    padding: 10px;
-    margin: 5px;
-    border: none;
-    border-radius: 5px;
+  background-color: #555;
+  color: #fff;
+  padding: 10px;
+  margin: 5px;
+  border: none;
+  border-radius: 5px;
 }
 
 .container_item:hover {
-    background-color: #777;
+  background-color: #777;
 }
 
 .container_item:focus {
-    outline: none;
-    box-shadow: 0 0 5px #fff;
+  outline: none;
+  box-shadow: 0 0 5px #fff;
 }
 </style>
